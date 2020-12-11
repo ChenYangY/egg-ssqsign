260d622f4f059f44578629ebe04f3139d18cd16c
chenyangyang <hn2cyy@163.com>
2020-12-11 14:25:20 +0800

    feat(main): egg 框架插件 ssqsign

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const assert = require('assert')
+const SSQSign = require('ssq-sign')
+const _ = require('lodash')
+module.exports = app => {
+  app.addSingleton('ssqSign', async function createSSQSign(config, app) {
+    assert(config.url && config.developerId && config.privateKey && config.publicKey && config.account);
+    app.coreLogger.info(`[egg-ssqsign]: ${config.url} ${config.developerId} ${config.account}`)
+    return new SSQSign(config.url, config.developerId, config.privateKey, config.publicKey, 
+        _.pick(config, 'account', 'timeout', 'retry', 'notifyUrl', 'returnUrl'))
+  })
+}
